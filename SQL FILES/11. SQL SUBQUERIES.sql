8eebedcc571019e11be7e21efcdf2ab641d2f271
VarshiniC28 <varshinic.parvam@gmail.com>
2025-06-24 18:05:53 +0530

    initial commit

--- SQL FILES/11. SQL SUBQUERIES.sql
+++ SQL FILES/11. SQL SUBQUERIES.sql
@@ -33,3 +33,44 @@ WHERE MARKS > (SELECT AVG(MARKS) FROM STUDENT);
 SELECT NAME , MARKS 
 FROM STUDENT , (SELECT AVG(MARKS) AS AVG_MARKS FROM STUDENT) AS AVG_TABLE
 WHERE MARKS > AVG_MARKS;
+
+-- 1. Writing subquery inside SELECT
+SELECT NAME ,marks, (SELECT AVG(MARKS) AS AVG_MARK FROM STUDENT)
+FROM STUDENT
+WHERE MARKS > AVG_MARK; -- Getting error
+
+WITH avg_table AS (
+  SELECT AVG(MARKS) AS avg_mark FROM STUDENT
+)
+SELECT NAME, MARKS FROM STUDENT, avg_table
+WHERE MARKS > avg_mark;
+-- -----------------------------------------------------------------------------------------------------------------------------
+
+-- Lets see another example:
+-- Find the names of all students with even roll numbers
+-- step 1: Find the even roll numbers
+-- step 2: Find the names of the students with even roll no.
+
+SELECT NAME FROM STUDENT
+WHERE ROLLNO % 2 = 0;
+
+-- OR Using the subquery as given below:
+SELECT NAME, ROLLNO FROM STUDENT
+WHERE ROLLNO IN (SELECT ROLLNO FROM STUDENT WHERE ROLLNO %2=0);
+-- ----------------------------------------------------------------------------------------------------------------------------
+
+-- Example with from and select :
+-- Find the max from the students of hassan
+-- step 1: Find the students of hassan
+-- step 2 : Find their max marks using the sublist in step 1.
+
+-- NORMAL QUERY
+SELECT MAX(MARKS) FROM STUDENT
+WHERE CITY = "HASSAN";
+
+-- SUBQUERY: from
+SELECT MAX(MARKS) FROM (SELECT * FROM STUDENT WHERE CITY = "HASSAN") AS MAX_TABLE;
+
+-- SELECT:
+SELECT (SELECT MAX(MARKS) FROM STUDENT WHERE CITY = "HASSAN") AS MAX_HASSAN;
+-- ----------------------------------------------------------------------------------------------------------
