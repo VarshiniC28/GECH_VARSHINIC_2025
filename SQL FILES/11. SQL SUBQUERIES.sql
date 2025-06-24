4ed77ba3710f6620c6e06ca7dbf710729dfe6705
VarshiniC28 <varshinic.parvam@gmail.com>
2025-06-24 16:57:44 +0530

    initial commit

--- SQL FILES/11. SQL SUBQUERIES.sql
+++ SQL FILES/11. SQL SUBQUERIES.sql
@@ -6,3 +6,30 @@
 -- SELECT cols FROM table_name
 -- WHERE col_name operator
 -- (subquery);
+
+-- There are 3 ways to write the subqurey:
+-- 1. Inside Select
+-- 2. Inside From
+-- 3. Inside where clause - Most useful with syntax written above.
+
+-- Example: 
+-- Get names of all students who scored more than class average.
+-- Step 1: Find the aavg of class
+-- Step 2: Find the names of students with marks>avg
+
+USE COLLEGE;
+SELECT * FROM STUDENT;
+ALTER TABLE STUDENT
+CHANGE COLUMN FULL_NAME NAME VARCHAR(50);
+
+-- 3. Writing subquery inside where clause:
+SELECT AVG(MARKS) FROM STUDENT; -- TO KNOW ONLY THE AVERAGE OF ALL MARKS.
+
+-- ANS: This below query is DYNAMIC QUERY not FIXED/STATIC since it gets updated automatically when there are any updations in table.
+SELECT NAME, MARKS FROM STUDENT
+WHERE MARKS > (SELECT AVG(MARKS) FROM STUDENT);
+
+-- 2. Writing subquery inside FROM:
+SELECT NAME , MARKS 
+FROM STUDENT , (SELECT AVG(MARKS) AS AVG_MARKS FROM STUDENT) AS AVG_TABLE
+WHERE MARKS > AVG_MARKS;
