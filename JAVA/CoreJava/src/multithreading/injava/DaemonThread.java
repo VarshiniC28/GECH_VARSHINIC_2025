72c68d92b564f81af7a06fb2a22aa2c78b08fd99
VarshiniC28 <varshinic.parvam@gmail.com>
2025-08-08 17:35:50 +0530

    Collection Framework

--- JAVA/CoreJava/src/multithreading/injava/DaemonThread.java
+++ JAVA/CoreJava/src/multithreading/injava/DaemonThread.java
@@ -1,10 +1,92 @@
 package multithreading.injava;
 
+class Virat extends Thread{
+	@Override
+	public void run() {
+		
+		BatCoach b = new BatCoach();
+		BowlCoach bo = new BowlCoach();
+		b.setName("Ba");
+		bo.setName("Bo");
+		
+		//This setDaemon(true) method makes the threads daemon when the argument is true and if its said false it will no longer be daemon thread
+		b.setDaemon(true); 
+		bo.setDaemon(true);
+		
+		//To check whether the thread is daemon thread or not
+		System.out.println(b.isDaemon()); //true
+		System.out.println(bo.isDaemon()); //true if they are daemon threads
+		
+		b.start();
+		bo.start();
+		
+		try {
+			System.out.println("Enters ground");
+			Thread.sleep(2000);
+			System.out.println("Do warmup");
+			Thread.sleep(2000);
+			System.out.println("Fielding");
+			Thread.sleep(2000);
+			System.out.println("bowling");
+			Thread.sleep(2000);
+			System.out.println("Batting");
+			System.out.println("Leave the ground");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class BatCoach extends Thread{
+	@Override
+	public void run() {
+		for(; ;) {
+			try {
+				System.out.println("Batting coach in the ground");
+				Thread.sleep(2000);
+			}catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+}
+
+class BowlCoach extends Thread{
+	@Override
+	public void run() {
+		for(; ;) {
+			try {
+				System.out.println("Bowling coach in the ground");
+				Thread.sleep(2000);
+			}catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+}
+
 public class DaemonThread {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+//		Daemon thread - Helper Thread for primary thread.. Eg: auto save in word which will do its work until wprd app is open
+		
+		//Identify daemon and primary thread, we want Thread-1 and 2 to execute only till primary thread i.e thread-0 is executing
+		
+		/* Rule :
+		 * 1. Identify daemon threads
+		 * 2. Primary thread should be the one to create the daemon threads - Create Objects of daemon threads and start them inside run method of primary thread as above
+		 * 3. Primary thread should use setDaemon() which takes boolean value as argument(set to true) and create Thread
+		 * 4. Must call setDaemon(true) before start()
+		 * So now the daemon threads execute until primary thread is executing
+		 * */
+		
+		Virat v = new Virat();
+		v.setName("V");
+		v.start();
+
+		
+		
 	}
 
 }
--- JAVA/CoreJava/src/multithreading/injava/ProducerConsumerInterThreadCommunication.java
+++ JAVA/CoreJava/src/multithreading/injava/ProducerConsumerInterThreadCommunication.java
@@ -3,15 +3,39 @@ package multithreading.injava;
 
 class Queue{
 	private int data;
+	boolean isDataPresent = false;
 
-	void setData(int data) {
-		this.data = data;
-		System.out.println("Producer produced the data: "+data);
+	//Whenever the resource/method has notify or wait we must make it synchronized - and using notify() and wait() is communication between threads
+	synchronized void setData(int data) { //only one thread can access this at any point of time
+		if(isDataPresent==false) {
+			this.data = data;
+			System.out.println("Producer produced the data: "+data);
+			isDataPresent = true;
+			notify(); //To notify that there is data to other threads we use this method, if we dont use notify, it may lead to dead lock or we could say there will be one 
+		}
+		else { //if the data is present and not empty then wait
+			try {
+				wait(); //if we dont use wait, there will be skip of lot of iterations
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}	
 	}
 
-	void getData() {
-		System.out.println(data);
-		System.out.println("Consumer consumed the data: "+data);
+	synchronized void getData() {
+		if(isDataPresent == true) {
+			System.out.println("Consumer consumed the data: "+data);
+			isDataPresent = false;
+			notify(); //notify()-any one of the thread will be back to RUNNABLE, notifyAll()- All threads will be be back
+
+		}
+		else { ///if data is not present yet , before u consume wait
+			try {
+				wait();
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
 
@@ -25,7 +49,7 @@ class Boy11 extends Thread{
 	public void run() {
 		int i=0;
 		for(; ;) { //for loop with just ; and no conditions is called infinite loop
-			q.setData(i++);
+			q.setData(i++); //there is difference as 2 4 6 because of processor speed its not possible for it to print
 		}
 	}
 }
@@ -60,6 +84,11 @@ public class ProducerConsumerInterThreadCommunication { //Producer and consumer 
 		
 		b.start();
 		g.start();
+		
+		
+		//Here the problem is , When Thread-0 is selected by Thread scheduler - it produces data like 1 2 3 4 - after its times over at last the data it has is 4
+		//Now when the Thread-1 is alloted with time, it keeps consuming only the last data present in data variable i.e 4 - so o/p will be 4 4 4
+		//There is con-current execution but what we want is produce 1  consume 1 , produce 2 consume 2 : so we use inter-thread communication
 	}
 
 }
