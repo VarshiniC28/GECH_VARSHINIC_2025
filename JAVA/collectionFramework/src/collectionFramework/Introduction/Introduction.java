e0a940a003b522d516c7d8e75d70ec53498ae9e5
VarshiniC28 <varshinic.parvam@gmail.com>
2025-08-08 23:52:45 +0530

    ArrayList'

---
+++ JAVA/collectionFramework/src/collectionFramework/Introduction/Introduction.java
@@ -0,0 +1,29 @@
+package collectionFramework.Introduction;
+
+import java.util.ArrayList;
+
+public class Introduction {
+
+	public static void main(String[] args) {
+
+		//Collection Framework: 
+		
+		ArrayList a = new ArrayList();
+		a.add(10);
+		a.add(20);
+		a.add(30);
+		a.add(30);
+		a.add(30);
+		a.add(30);
+		a.add(30);
+		a.add(30);
+		a.add(30);
+		a.add(30);
+		a.add(30);
+		
+		
+		//no need of for loop	
+		System.out.println(a);
+	}
+
+}
